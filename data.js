24410fa93a409a6035b4e2bdd4158c51342ddcff
BL091 <benjamin24037@gmail.com>
2024-12-11 13:41:01 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,79 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-couloir",
+      "name": "Couloir",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -0.2464126336742254,
+        "pitch": 0.1169366572965771,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7717144610393678,
+          "pitch": 0.11593884595076354,
+          "rotation": 0,
+          "target": "1-bureau-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bureau-2",
+      "name": "Bureau 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -0.5092193541176258,
+        "pitch": 0.2550326218046486,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.22499435750743757,
+          "pitch": 0.21712276839205735,
+          "title": "Bureau des référents numériques<br>",
+          "text": "<div>M.LEBER</div><div>M.COMPAGNON</div><div>M.PLANCHAIS<br></div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
